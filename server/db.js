200cda0f275d2205fc9c4262ea3be5441a4009f0
mats <172794@student.horizoncollege.nl>
2026-03-25 10:31:32 +0100

    een error opgelost voor de api

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'bit-academy',
-  password: 'bit-academy',
+  host: '127.0.0.1',
+  user: 'bit_academy',
+  password: 'bit_academy',
   database: 'middenbeemster_Smidse'
 });
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet'); // toevoegen
 
 const app = express();
 
-app.use(cors()); // nodig voor Vue
+// Security headers (CSP)
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "data:", "http://localhost:3000"],
+      connectSrc: ["'self'", "http://localhost:3000"],
+    },
+  })
+);
+
+app.use(cors()); // voor vue
 app.use(express.json());
 
+// Routes
 const contentRoutes = require('./routes/content');
 const authRoutes = require('./routes/auth');
 
 app.use('/api/content', contentRoutes);
 app.use('/api/auth', authRoutes);
+app.get('/', (req, res) => {
+  res.send('API werkt 🚀');
+});
 
+// Server starten
 app.listen(3000, () => {
   console.log('API draait op http://localhost:3000');
 });
