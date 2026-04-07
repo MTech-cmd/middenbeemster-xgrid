e6d900ea068208fb3c5b47c873b558e60c3919dc
Mehdi El Khallouki <mehdi-elkhallouki@pm.me>
2026-04-07 10:55:30 +0200

    test

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: '127.0.0.1',
+  host: '127.0.0.1',   // 👈 belangrijk!
   user: 'bit_academy',
   password: 'bit_academy',
-  database: 'middenbeemster_smidse'
+  database: 'middenbeemster_Smidse',
+  connectionLimit: 10
 });
 
 module.exports = pool;
