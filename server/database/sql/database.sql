f007ecac7027852ad2d3f6d6379b6aa3b16cd47a
mats <172794@student.horizoncollege.nl>
2026-04-15 15:32:34 +0200

    database aangepast voor de uitgebrijdere adminpage dat jasper wou, very basic html voor de adminpage gemaakt

--- server/database/sql/database.sql
+++ server/database/sql/database.sql
@@ -10,6 +10,33 @@ CREATE TABLE UserAdmin (
     role ENUM('admin') DEFAULT 'admin'
 );
 
+CREATE TABLE Pages (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Texmplate VARCHAR(255) NOT NULL,
+    Routing VARCHAR(255) NOT NULL UNIQUE,
+    PublishedBy varchar(255) NOT NULL,
+    lastEditedBy varchar(255),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(id),
+    FOREIGN KEY (lastEditedBy) REFERENCES UserAdmin(id),
+    FOREIGN KEY (id) REFERENCES Content(id)
+);
+
+CREATE TABLE Navbar (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(255) NOT NULL,
+    Link VARCHAR(255) NOT NULL,
+    PublishedBy varchar(255) NOT NULL,
+    lastEditedBy varchar(255),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(id),
+    FOREIGN KEY (lastEditedBy) REFERENCES UserAdmin(id)
+);
+
 CREATE TABLE Content (
     id INT PRIMARY KEY AUTO_INCREMENT,
     Location VARCHAR(255) NOT NULL,
@@ -21,6 +48,6 @@ CREATE TABLE Content (
     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(username),
-    FOREIGN KEY (lastEditedBy) REFERENCES UserAdmin(username)
+    FOREIGN KEY (PublishedBy) REFERENCES UserAdmin(id),
+    FOREIGN KEY (lastEditedBy) REFERENCES UserAdmin(id)
 );
